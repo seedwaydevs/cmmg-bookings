d391ada3c470e1e9d6dc36df58407213902a71c2
Rorisang Putu <rorisangputu@gmail.com>
2026-02-07 19:01:59 +0200

    adding images

---
+++ src/lib/imageData.ts
@@ -0,0 +1,22 @@
+import studio1 from "../../public/hire/studio-1.jpg";
+import studio2 from "../../public/hire/studio-2.jpg";
+import studio3 from "../../public/hire/studio-3.jpg";
+import studio4 from "../../public/hire/studio-4.jpg";
+import studio5 from "../../public/hire/studio-5.jpg";
+import studio6 from "../../public/hire/studio-6.jpg";
+import studio7 from "../../public/hire/studio-7.jpg";
+import studio8 from "../../public/hire/studio-8.jpg";
+
+import logo from "../../public/cmmg-logo.png";
+
+export {
+  studio1,
+  studio2,
+  studio3,
+  studio4,
+  studio5,
+  studio6,
+  studio7,
+  studio8,
+  logo,
+};
