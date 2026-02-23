50f5f8a8461c216148844b1e4c463423522c47c4
Rorisang Putu <rorisangputu@gmail.com>
2026-02-24 01:33:41 +0200

    removing db url console log

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,6 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
